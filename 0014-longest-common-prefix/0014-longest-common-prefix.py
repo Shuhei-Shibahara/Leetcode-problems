a4717281547ccad8b6227bd17fd5b7eecc4c3cdb
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-09-12 14:38:59 -0700

    Time: 40 ms (40.72%), Space: 16.5 MB (95.4%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,10 +1,12 @@
 class Solution:
+    #compare strings to each other with index
+    #when you find the a letter thats out of place then return that string
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        base = strs[0]
-        for i in range(len(base)):
+
+        check = strs[0]
+        for i in range(len(check)):
             for word in strs[1:]:
-                if i == len(word) or word[i] != base[i]:
-                    return base[0:i]
-            
-        return base
+                if i == len(word) or check[i] != word[i]:
+                    return word[0:i]
+        return check
         
