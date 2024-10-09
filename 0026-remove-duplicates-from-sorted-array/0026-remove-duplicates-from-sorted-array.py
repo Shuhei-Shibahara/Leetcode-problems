02096c9e296b9d9044073d5a01eb33bbf1f0c882
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-10-08 21:07:03 -0700

    Time: 80 ms (32.63%), Space: 18.1 MB (13.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,15 +1,11 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> List[int]:
-        #return k elements that they are gonna check
-        #use 2 pointer method that way we can keep track of how many indexes to check
-        #we must change the array nums
-
-        if not nums:
-            return 0
-
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #were going to iterate through nums to check if the previous value was equal 
+        #if we find one that doesnt equal we replace that value with an index checker that we will initailize
+        
         j = 1
 
-        for i in range(1, len(nums)):
+        for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
                 nums[j] = nums[i]
                 j += 1
