d2444043dbe81591369733660ca52b8dfc75a4f6
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-11-12 19:41:51 -0800

    Time: 0 ms (100%), Space: 17.9 MB (48.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,15 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        #2pointer 
-        #l pointer will be moving once if finds a new non duplicate number
-        #r pointer will be the one searching for a new number that is non duplicate
+        #were going to have the array have no duplicates
+        #2 pointer
+        #l keep track of indicies only move when its not a duplicate
+        #r pointer will keep checking with the left pointer to see if ther are duplicates
 
         l = 0
-
         for r in range(1,len(nums)):
-
-            if nums[r] != nums[l]:
+            if nums[l] != nums[r]:
                 l += 1
                 nums[l] = nums[r]
-
+        
         return l + 1
+
+        
