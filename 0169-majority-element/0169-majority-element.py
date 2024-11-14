0a3182a0b34c3bd6ff16b32449540047a46325fa
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-11-14 14:36:55 -0800

    Time: 7 ms (57.8%), Space: 18.5 MB (9.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-
-        return nums[len(nums)// 2]
+        count = Counter(nums)
+        # Return the key with the highest value
+        return max(count, key=count.get)
