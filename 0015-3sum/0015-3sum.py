a8e3cbcca0f35c6b15a97d70691ae58d90335543
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-12-16 18:47:54 -0800

    Time: 393 ms (97.66%), Space: 20.2 MB (34.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #sort nums to use for loop 
-        #intialize l and r values 
-        #check if they are all unique values 
-        #if they are and their sum equals 0 then we will put them into an array 
-        #we will return that array
-
+        res = []
         nums.sort()
-        result = []
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
-            l = i + 1
-            r = len(nums) - 1
+
+            l, r = i + 1, len(nums) - 1
             while l < r:
-                total = nums[i] + nums[l] + nums[r]
-                if total < 0:
-                    l += 1
-                elif total > 0:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
                     r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    result.append([nums[i],nums[l],nums[r]])
-                    while l < r and nums[l] == nums[l+1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
+                    res.append([a, nums[l], nums[r]])
                     l += 1
                     r -= 1
-        return result
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                        
+        return res
