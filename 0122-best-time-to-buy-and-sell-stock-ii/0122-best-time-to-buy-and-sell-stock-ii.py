132a93bcc95ebee2dbd40f3777f6c4cd58c371de
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-12-27 14:39:15 -0800

    Time: 0 ms (100%), Space: 18.9 MB (12.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        #no matter what you do if you just subtract the difference between the number of one less itll make sense
+        
         profit = 0
         
         for i in range(1, len(prices)):
