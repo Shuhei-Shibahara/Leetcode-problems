3d06ac68d718113bb0b420c9665b5adf0a4be0eb
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2024-12-30 20:01:47 -0800

    Time: 0 ms (100%), Space: 18 MB (8.45%) - LeetHub

--- 0983-validate-stack-sequences/0983-validate-stack-sequences.py
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.py
@@ -1,5 +1,10 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        #we will use stack for this
+        #run push commands and fill the array
+        #if the last number on the stack is the popped first number then we can pop
+        #if the stack is empty then we will return True
+        
         stack = []
         j = 0
         for i in pushed:
