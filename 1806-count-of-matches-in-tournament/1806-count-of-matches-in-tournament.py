bffa3bb6a232d836d81b9523bb1099e34d7fb138
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2025-01-04 22:32:52 -0800

    Time: 45 ms (6.93%), Space: 17.8 MB (6.19%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n > 1:
+            if n % 2 == 0:
+                n = n//2
+                matches += n
+            else:
+                matches += (n - 1) // 2 + 1
+                n = (n - 1) // 2
+
+        
+        return matches
+                
