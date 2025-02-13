fee2d023ba6c0619d2353f343df91ac19f21ede9
Shuhei Shibahara <110438117+Shuhei-Shibahara@users.noreply.github.com>
2025-02-13 15:03:51 -0800

    Time: 9 ms (79.14%), Space: 17.7 MB (81.2%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        
+        #XOR formula is 0 ^ key
         def dfs(i, total):
             if i == len(nums):
                 return total
